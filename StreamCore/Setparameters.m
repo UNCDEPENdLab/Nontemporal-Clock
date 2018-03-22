fc15430877760d08c3254f2f1d506920de1bc5f5
PsychMike <mrhess25@gmail.com>
2018-03-22 16:01:28 -0400

    Single monitor fix

--- StreamCore/Setparameters.m
+++ StreamCore/Setparameters.m
@@ -65,7 +65,7 @@ Parameters.datafilename = 'ExpSub';    %data file prefix
 % if sum(Screen('Screens'))
 %     Parameters.whichscreen = 2;
 % else
-    Parameters.whichscreen = 2;  %which of the displays do we use?  (only relevant if there are multiple monitors)
+    Parameters.whichscreen = 0;  %which of the displays do we use?  (only relevant if there are multiple monitors)
 % end
 backcolor = 255;
 Parameters.backgroundcolor = repmat(backcolor,1,3);   %what color is the background screen in RGB (0-255)
