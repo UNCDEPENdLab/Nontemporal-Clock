81a866c44285a410d8db59f7b62770563f21dff1
PsychMike <mrhess25@gmail.com>
2018-04-12 22:43:21 -0400

    run_clock_task.m now waits for N key before continuing to exp.

--- run_clock_task.m
+++ run_clock_task.m
@@ -1,3 +1,10 @@
 eval('clear all');
-eval('web(''digit-span/index.html'')');
+eval('web(''digit-span/index.html'',''-notoolbar'')');
+run_task = 0;
+while run_task == 0
+    [isDown, secs, keyCode, deltaSecs] = KbCheck;
+    if find(keyCode==1)==KbName('N')
+        run_task = 1;
+    end
+end
 Runexp;
